f8c2cdf1b1cb06ed3b5729c8cadf339b9d9efe89
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-03-24 18:24:43 +1100

    cleaned up the file

--- vowel_counter.rb
+++ vowel_counter.rb
@@ -6,7 +6,6 @@
 
 # For example if the user enters "All cows eat grass", it would output "5").
 # ========================================================================== //
-require 'pry'
 
 vowels = ["a", "e", "i", "o", "u"]
 vowel_count = []
@@ -19,54 +18,3 @@ str.each_char do |w|
  end
 end
 vowel_count.length
-
-# str.each_char.any? {|w| vowels.include?(w) }
-
-
-# str.each_char do |c| 
-# 	if vowels.include?(c) 
-# 		puts c 
-# 	end
-# end
-
-
-# user_input = gets.chomp.to_s
-# vowels.any? { |w| user_input.include?( vowels ) }
-
-# user_input = gets.chomp.to_s
-# user_input.each_char do |c| 
-# 	if vowels.include?(c) 		
-# 		vowel_count << c
-# 	end	
-# end
-# binding.pry
-# vowel_count.length
-
-
-# def get_input
-# 	puts "Enter a String"
-# 	@user_input = gets.chomp
-# end
-
-# def convert_input_to_array
-# 	@user_input_letters = []
-
-# 	@user_input.each do |letter|
-# 		@user_input_letters << letter 
-# 	end 
-# 	return @user_input_letters
-# end
-
-# def check_for_vowels
-# 	@user_input.each do |letter|
-# 		if letter.include?(vowels)
-# 			vowel_count << letter
-# 		end
-# 		return vowel_count.length
-# 	end
-# end
-
-# get_input
-# convert_input_to_array
-# check_for_vowels
-
